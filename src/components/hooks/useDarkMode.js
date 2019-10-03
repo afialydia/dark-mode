d98d3c83c6f8b71907aa356629609172fc16115b
Afia-Lydia <you@example.com>
2019-10-03 18:57:04 -0400

    updates with tl

---
+++ src/components/hooks/useDarkMode.js
@@ -0,0 +1,27 @@
+import useLocalStorage from './useLocalStorage';
+import {useEffect} from 'react'
+
+const useDarkMode = (key) => { 
+    const [value, setStoredValue] = useLocalStorage(key);
+
+    useEffect(() => {
+        if(value){
+           document.body.classList.add("dark-mode");
+        }
+        else{
+            document.body.classList.remove("dark-mode");
+        }
+
+    }, [value]);
+
+    const setValue = value => {
+
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value))
+
+    }
+
+    ​ return [value, setValue];
+}
+
+export default useDarkMode;
---
+++ src/components/hooks/useLocalStorage.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+const useLocalStorage = (key, initialValue) => {
+   
+    // if(typeof key !== 'string'){
+    //     throw new Error( 'invalid Params: useLocalStorage needs string' )
+    // };
+     
+    const [storedValue, setStoredValue] = useState(() => {
+        const item = window.localStorage.getItem(key);
+        return item ? JSON.parse(item) : initialValue;
+    });
+    
+
+    const setValue = value => { 
+    
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value))
+    }
+
+    return [storedValue, setValue];
+}
+
+export default useLocalStorage;
